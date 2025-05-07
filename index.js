8183689a669a46cdf3e4779a77bd80850db295e7
Israa6yazbeck <151446813+Israa6yazbeck@users.noreply.github.com>
2025-05-07 01:52:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,53 @@
-var o = document.querySelectorAll(".drum").length;
+// Select all elements with the class 'drum'
+var drumButtons = document.querySelectorAll(".drum");
 
-for (var i = 0; i < o; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    var inner = this.innerHTML;
-    makesound(inner);
+// Loop through each drum button and add a click event listener
+for (var i = 0; i < drumButtons.length; i++) {
+  drumButtons[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
   });
 }
 
+// Add a keypress event listener to the entire document
 document.addEventListener("keypress", function (event) {
-  makesound(event.key);
+  makeSound(event.key);
 });
 
-function makesound(key) {
+// Function to play the corresponding sound based on the key
+function makeSound(key) {
   switch (key) {
     case "w":
-      new Audio("sounds/tom-1.mp3").play();
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
       break;
     case "a":
-      new Audio("sounds/tom-2.mp3").play();
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
       break;
     case "s":
-      new Audio("sounds/tom-3.mp3").play();
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
       break;
     case "d":
-      new Audio("sounds/tom-4.mp3").play();
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
       break;
     case "j":
-      new Audio("sounds/snare.mp3").play();
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
       break;
     case "k":
-      new Audio("sounds/crash.mp3").play();
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
       break;
     case "l":
-      new Audio("sounds/kick-bass.mp3").play();
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
       break;
     default:
       console.log(key);
   }
 }
+
+    
